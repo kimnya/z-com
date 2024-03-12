037964ca7fef9fdd7016a39b160e98cc5ce56c97
김냐 <ymca582@icloud.com>
2024-03-12 21:30:42 +0900

    design:시작화면 UI 디자인

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        로그아웃 레이아웃
+        {children}
+      </body>
     </html>
   );
 }
